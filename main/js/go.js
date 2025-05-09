912334b13206a5b3b58b7804b81dd62b3e6f3f28
saphire-gamez <saphiregamezz@gmail.com>
2025-05-08 22:19:01 -0400

    Update go.js

--- main/js/go.js
+++ main/js/go.js
@@ -139,7 +139,7 @@ function uv(url) { // Open Ultraviolet
     if(url != ""){
       if(localStorage.getItem("ab_cloak") == "true") {
         var tab = window.open('about:blank', '_blank');
-        tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>' + /*(localStorage.getItem("tabCloakTitle") ? localStorage.getItem("tabCloakTitle") : "Utopia")*/'Utopia' + '</title><link rel="icon" type="image/png" href="' + /*(localStorage.getItem("tabCloakIcon") ? localStorage.getItem("tabCloakIcon") : window.location.origin + "/favicon.ico")*/window.location.origin + "/favicon.ico" + '"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + __uv$config.prefix + encodeXor(this.url) + '" frameborder="0"></iframe></body></html>';
+        tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>' + /*(localStorage.getItem("tabCloakTitle") ? localStorage.getItem("tabCloakTitle") : "Utopia")*/'Algebra-lessons.com' + '</title><link rel="icon" type="image/png" href="' + /*(localStorage.getItem("tabCloakIcon") ? localStorage.getItem("tabCloakIcon") : window.location.origin + "/favicon.ico")*/window.location.origin + "/favicon.ico" + '"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + __uv$config.prefix + encodeXor(this.url) + '" frameborder="0"></iframe></body></html>';
         tab.document.close();
     
         window.top.location.replace("https://www.google.com");
